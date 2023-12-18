9fdc96e0784bc1ab3a4e2616b16b6a33b98eb1b4
elias-dzobo <elias.dzobo@gmail.com>
2023-12-18 13:54:24 +0000

    add to do list function

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 from langchain.tools import tool
 import requests as re 
-
+from todoist_api_python.api import TodoistAPI
 
 
 @tool 
@@ -18,3 +18,21 @@ def get_crypto_updates(token: str) -> str:
 
 
     return token['usd'] 
+
+def add_todo(todo: str):
+    """
+    useful when user wants to add an item to their to do list 
+    """
+
+    api = TodoistAPI('c861b26a39eabe6b4c7f1295bf8ce4029eb630f9')
+
+    try:
+        task = api.add_task(content=todo, project_id='2325260660')
+        print(task)
+
+    except Exception as error:
+        print(error)
+    
+
+if __name__ == '__main__':
+    add_todo('Go to the gym')
--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from langchain.agents.format_scratchpad import format_to_openai_function_message
 from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
 import requests as re
 import json
+from todoist_api_python.api import TodoistAPI
+from langchain.memory import ConversationBufferMemory
 
 _ = load_dotenv(find_dotenv())
 openai.api_key = os.environ['OPENAI_API_KEY']
@@ -45,10 +47,35 @@ def get_crypto_updates(token: str) -> json:
 
     return json_form[token]['usd']
 
+@tool
+def add_todo(todo: str):
+    """
+    useful when user wants to add an item to their to do list 
+    """
+
+    api = TodoistAPI('c861b26a39eabe6b4c7f1295bf8ce4029eb630f9')
+
+    try:
+        task = api.add_task(content=todo, project_id='2325260660')
+        #memory.save_context(task) 
+
+        print('Task Added')
+
+        return task
+
+    except Exception as error:
+        
+        return error
+    
+    
+
+
+
 tools = [
-  get_crypto_updates
+  get_crypto_updates, add_todo
 ]
 
+memory = ConversationBufferMemory(memory_key="db")
 
 
 llm_with_tools = llm.bind(functions=[format_tool_to_openai_function(t) for t in tools])
@@ -78,6 +105,6 @@ echo = (
     | OpenAIFunctionsAgentOutputParser()
 )
 
-agent_executor = AgentExecutor(agent=echo, tools=tools, verbose=True)
+agent_executor = AgentExecutor(agent=echo, tools=tools, verbose=True, memory=memory)
 
-agent_executor.invoke({"input": "what is the current price of the cryptocurrency solana ?"})
+agent_executor.invoke({"input": "what is the Task id for Code a game?"})
